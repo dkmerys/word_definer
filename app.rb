04fb58b43a78bc6221f97b68b21a48744e5d33c1
Daniel Merys <dkmerys@gmail.com>
2020-05-15 10:52:43 -0700

    add delete functionality to route

--- app.rb
+++ app.rb
@@ -47,3 +47,10 @@ patch('/words/:id') do
   @words = Word.all
   erb(:words)
 end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete
+  @words = Word.all
+  erb(:words)
+end
