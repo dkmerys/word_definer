f0715dea3c64621d7baffdfb0a8265ce90ba8b49
Daniel Merys <dkmerys@gmail.com>
2020-05-15 08:45:24 -0700

    pass save function test, add/pass clear test

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,1 +1,27 @@
-*
+class Word
+  attr_accessor :name, :id
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values()
+  end
+
+  def self.clear
+    @@words = {}
+    @@total_rows = 0
+
+  def save
+    @@words[self.id] = Word.new({:name => self.name, :id => self.id})
+  end
+
+  def ==(word_to_compare)
+    self.name() == word_to_compare.name()
+  end
+end 
+end
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -3,6 +3,10 @@ require 'pry'
 require 'word_definer'
 
 describe '#Word' do
+
+  before(:each) do
+    Word.clear
+  end
   
   describe('#save') do
     it("saves a word") do
@@ -13,4 +17,21 @@ describe '#Word' do
       expect(Word.all).to(eq([word, word2]))
     end
   end
-end
+
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all words") do
+      word = Word.new({:name => "Tree", :id => nil})
+      word.save
+      word2 = Word.new({:name => "House", :id => nil})
+      word2.save
+      Word.clear
+      expect(Word.all).to(eq([]))
+    end
+  end
+end 
