78eea885174ec2816f95e3816b664726340f8ad4
Daniel Merys <dkmerys@gmail.com>
2020-05-15 16:26:57 -0700

    pass edit a definition test

--- spec/word_definer_integration_spec.rb
+++ spec/word_definer_integration_spec.rb
@@ -23,3 +23,17 @@ describe('create a definition path', {:type => :feature}) do
     expect(page).to have_content('a plant with a trunk')
   end
 end
+
+describe('edit a definition path', {:type => :feature}) do
+  it('creates an word, goes to the definition page, then edits a definition') do
+    word = Word.new({:name => "Tree", :id => nil})
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('new_def', :with => 'a plant with a trunk')
+    click_on('Add definition')
+    click_on('a plant with a trunk')
+    fill_in('edit_def', :with => 'brown trunk green leaves')
+    click_on('Change')
+    expect(page).to have_content('brown trunk green leaves')
+  end
+end
