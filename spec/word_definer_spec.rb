6eed50be7350d134ad46b57b8c7591d93955545b
Daniel Merys <dkmerys@gmail.com>
2020-05-15 08:27:28 -0700

    add save function test

--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -1,1 +1,16 @@
-*
+require 'rspec'
+require 'pry'
+require 'word_definer'
+
+describe '#Word' do
+  
+  describe('#save') do
+    it("saves a word") do
+      word = Word.new({:name => "Tree", :id => nil})
+      word.save
+      word2 = Word.new({:name => "House", :id => nil})
+      word2.save
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
+end
