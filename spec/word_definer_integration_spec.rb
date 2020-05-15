968eb0d38df4d051d365d6fec2b0d2e2a1fa4835
Daniel Merys <dkmerys@gmail.com>
2020-05-15 16:19:33 -0700

    pass create a definition test

--- spec/word_definer_integration_spec.rb
+++ spec/word_definer_integration_spec.rb
@@ -12,3 +12,14 @@ describe('create an word path', {:type => :feature}) do
     expect(page).to have_content('Reggae')
   end
 end
+
+describe('create a definition path', {:type => :feature}) do
+  it('creates an word and then goes to the definition page') do
+    word = Word.new({:name => "Tree", :id => nil})
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('new_def', :with => 'a plant with a trunk')
+    click_on('Add definition')
+    expect(page).to have_content('a plant with a trunk')
+  end
+end
